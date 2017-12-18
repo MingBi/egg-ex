9466f1c50b957ffcc253a23abe5df32e6f1b8094
MingBi <hhwwyy334@126.com>
2017-12-18 15:13:01 +0800

    添加注释

--- app/middleware/bejson.js
+++ app/middleware/bejson.js
@@ -1,13 +1,18 @@
+//返回数据格式化处理
 module.exports = () => {
   return async function bejson(ctx,next) {
     try {
       await next();
 
+      //如果返回的是文件
       if (Buffer.isBuffer(ctx.body)) return;
 
+      //如果没有返回
       if (!ctx.body) {
         ctx.body = '';
       }
+
+      //返回格式化
       ctx.body = {
         code: 0,
         status: 'success',
@@ -15,6 +20,7 @@ module.exports = () => {
       };
     } catch (error) {
 
+      //如果之前任意环节报错
       ctx.body = {
         code: 1,
         status: 'failed',
