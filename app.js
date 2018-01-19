7f6378c39e0129f5b0b5a298aab60713c7b76bfb
MingBi <hhwwyy334@126.com>
2018-01-19 16:35:42 +0800

    添加连接池注释

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ module.exports = app =>{
   app.beforeStart(async () =>{
     app.logger.debug('the config is ' + app.config.name);
 
+    //创建一个连接池，具体参数见config，max为最大连接数
     app.pool_yushan = new pg.Pool(app.config.yushan);
 
     app.sequelize=new Sequelize(app.config.yushan.database,app.config.yushan.user,app.config.yushan.password,{
@@ -25,19 +26,24 @@ module.exports = app =>{
   });
 
 
+  //封装一个执行sql语句方法
   app.yushanQuery = function(sql, param) {
     return new Promise(function(resolve, reject) {
+      //在连接池取出一个链接
       app.pool_yushan.connect(function(err, client, done) {
         if (err) {
           return console.error('error fetching client from pool', err);
         }
+        //执行sql语句
         client.query(sql, param, function(error, results) {
+          //断开与连接池的链接，如果不断开，这个链接将被占用
           done();
           if (error) {
             console.log(sql);
             console.log(param);
             return reject(error);
           }
+          //返回查询结果
           resolve(results.rows);
         });
       });
